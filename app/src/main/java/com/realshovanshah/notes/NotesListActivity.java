068a1efe94f7dd682b8e42a91da24f870a6f0cc2
Shovan Shah <realshovanshah@gmail.com>
2020-08-14 21:14:51 +0545

    Setting up project

---
+++ app/src/main/java/com/realshovanshah/notes/NotesListActivity.java
@@ -0,0 +1,22 @@
+package com.realshovanshah.notes;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import com.realshovanshah.notes.models.Note;
+
+public class NotesListActivity extends AppCompatActivity {
+
+    private static final String TAG = "NotesListActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notes_list);
+
+        Note note = new Note("some title", "some content for the note", null);
+
+        Log.d(TAG, "onCreate: my first note: " + note.toString());
+    }
+}
